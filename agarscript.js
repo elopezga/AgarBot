6d99acbe19c463db418d2e7c3914d965f3f03e25
Edgar Lopez <edgarylop@gmail.com>
2015-05-09 11:36:55 -0700

    Initial commit

---
+++ agarscript.js
@@ -0,0 +1,16 @@
+javascript: (function() {
+    window.open('Hi');
+})();
+
+
+/*
+ * To install:
+ * Create a bookmark with link:
+ *
+ * javascript: (function() {
+ *		var s = document.createElement('script');
+ *		s.type = 'text/javascript';
+ *		document.body.appendChild(s);
+ *		s.src = '/home/hogart/Desktop/agarscript.js';
+ * })();
+ */
